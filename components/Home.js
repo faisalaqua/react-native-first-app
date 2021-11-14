559e56fd1782dacf7a10a1214f9bd47ab34d38ee
Your Name <you@example.com>
2021-11-14 20:13:33 +0300

    foundations

---
+++ components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { NativeBaseProvider } from "native-base";
+import { Text } from "react-native";
+
+export default function Home() {
+  return <Text>Hello World</Text>;
+}
